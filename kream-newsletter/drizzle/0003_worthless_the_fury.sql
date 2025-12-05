0cfb670339f18e430fa6cfbb960aa8434c142799
gptunique <131907118+gptunique@users.noreply.github.com>
2025-12-05 09:43:50 +0900

    Delete kream-newsletter directory

--- kream-newsletter/drizzle/0003_worthless_the_fury.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `userAlerts` MODIFY COLUMN `thresholdPercent` int DEFAULT 10;--> statement-breakpoint
-ALTER TABLE `userAlerts` ADD `alertType` enum('percent_change','price_below','price_above') DEFAULT 'percent_change' NOT NULL;--> statement-breakpoint
-ALTER TABLE `userAlerts` ADD `targetPrice` int;
--- kream-newsletter/drizzle/0004_hot_bloodstrike.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `userAlerts` MODIFY COLUMN `thresholdPercent` int;
